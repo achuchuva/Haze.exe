bf5df3c88f5d426375940aee3a3d22b074265fce
Anton Chuchuva <anton@chuchuva.com>
2025-10-31 11:21:42 +1100

    Just a normal commit :)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
     public static Gamemanager Instance;
     bool displayedWASD = false;
+    public GameObject[] hazeEffects;
+    bool hazeEnabled = true;
+    bool hazeTogglePressed = false;
+    bool levelSkipPressed = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,6 +47,50 @@ public class Gamemanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Z) && Input.GetKey(KeyCode.M))
+        {
+            if (!hazeTogglePressed)
+            {
+                hazeEnabled = !hazeEnabled;
+                if (!hazeEnabled)
+                {
+                    WarningFlash.Instance.FlashWarning("HAZE EFFECTS DISABLED", 40);
+                    GlitchManager.Instance.DisableEffects();
+                }
+                else
+                {
+                    WarningFlash.Instance.FlashWarning("HAZE EFFECTS ENABLED", 40);
+                }
+                foreach (GameObject haze in hazeEffects)
+                {
+                    haze.SetActive(hazeEnabled);
+                }
+                hazeTogglePressed = true;
+            }
+        }
+        else
+        {
+            hazeTogglePressed = false;
+        }
+
+        if (Input.GetKey(KeyCode.B) && Input.GetKey(KeyCode.Y) && Input.GetKey(KeyCode.E))
+        {
+            if (!levelSkipPressed)
+            {
+                if (SceneManager.GetActiveScene().name == "Level 1")
+                {
+                    LevelLoader.Instance.LoadLevel("Level 2");
+                }
+                else if (SceneManager.GetActiveScene().name == "Level 2")
+                {
+                    LevelLoader.Instance.LoadLevel("Level 3");
+                }
+                levelSkipPressed = true;
+            }
+        }
+        else
+        {
+            levelSkipPressed = false;
+        }
     }
 }
